50b974552a54aa5264e9a9653309aed023671c95
caihongwei <2024212657@bupt.edu.cn>
2024-12-12 13:44:57 +0800

    vue框架初试

---
+++ vuel/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Person from './components/Person.vue'
+// No need for HelloWorld if you don't want it right now.
+</script>
+
+<template>
+  <div class="app">
+    <h1>Hello from the other side!</h1>
+    <!-- Person component is displayed below -->
+    <Person />
+  </div>
+</template>
+
+<style scoped>
+.app {
+  padding: 20px;
+  font-family: sans-serif;
+}
+</style>
+
---
+++ vuel/src/components/Person.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="person">
+      <h2>你好啊！</h2>
+      <p>Name: {{ name }}</p>
+      <p>Age: {{ age }}</p>
+      <p>Tel: {{ tel }}</p>
+      <button @click="showData">Show Data</button>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue'
+  
+  // Define component data
+  const name = ref('CHW')
+  const age = ref(18)
+  const tel = ref('123123')
+  
+  function showData() {
+    alert(`Data: ${name.value}, ${age.value}, ${tel.value}`)
+  }
+  </script>
+  
+  <style scoped>
+  .person {
+    background-color: skyblue;
+    padding: 10px;
+    width: 200px;
+  }
+  </style>
+  
